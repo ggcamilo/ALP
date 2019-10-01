476c1403efe7c6acfe0acb1cece4f702734d251a
ggcamilo <camilogg1996@hotmail.com>
2019-10-01 14:45:22 -0300

    Subo soluciones a todos los ejercicios.

--- TP2/Untyped.hs
+++ TP2/Untyped.hs
@@ -11,13 +11,13 @@ import Common
 -- Ejercicio 2: Conversión de Términos
 ----------------------------------------------
 
-getLevel :: [(String, Int)] -> String -> Maybe Int
+getLevel :: NameEnv Int -> String -> Maybe Int
 getLevel s v = case ns of
                  [] -> Nothing
                  _  -> Just (foldr1 max ns)
   where ns = [m | (u, m) <- s, u == v]
 
-conversion' :: Int -> [(String, Int)] -> LamTerm -> Term
+conversion' :: Int -> NameEnv Int -> LamTerm -> Term
 conversion' n s (LVar v)    = case (getLevel s v) of
                                 Just m  -> Bound m
                                 Nothing -> Free v
@@ -35,32 +35,35 @@ conversion = conversion' (-1) []
 -------------------------------
 
 shift :: Term -> Int -> Int -> Term
-shift (Free v)    c d = Free v
-shift (Bound k)   c d = if k < c then (Bound k)
-                                 else (Bound (k+d))
-shift (t1 :@: t2) c d = (shift t1 c d) :@: (shift t2 c d)
+shift (Free v)    _ _ = Free v
+shift (Bound k)   c d = if k < c then (Bound k) else (Bound $ k+d)
 shift (Lam t)     c d = Lam $ shift t c d
-  
-  
-subst' :: Term -> Term -> Int -> Int -> Term
+shift (t1 :@: t2) c d = (shift t1 c d) :@: (shift t2 c d)
+
+
+subst' :: Term -> Term -> Int -> Int-> Term
 subst' (Free v)    _  _ _ = Free v
-subst' (Bound k)   t' i j = if k < i then (Bound k)
-                              else if k > i then (Bound (k-1))
-                                     else (shift t' i j)
-subst' (t1 :@: t2) t' i j = (subst' t1 t' i j) :@: (subst' t2 t' i j)
+subst' (Bound k)   t' i j = if k == i then (shift t' i j) else 
+                               if k > i then (Bound $ k-1) else (Bound k)
 subst' (Lam t)     t' i j = Lam $ subst' t t' i (j+1)
+subst' (t1 :@: t2) t' i j = (subst' t1 t' i j) :@: (subst' t2 t' i j)
 
 subst :: Term -> Term -> Int -> Term
 subst t t' i = subst' t t' i 0
 
 
 eval' :: NameEnv Term -> Int -> Term -> Term
-eval' = undefined
+eval' s n (Free v)    = case lookup v s of
+                          Nothing -> Free v
+                          Just t  -> shift t 0 (n+1)
+eval' _ _ (Bound k)   = Bound k
+eval' s n ((Lam t1) :@: t2) = eval' s n t
+  where t = subst t1 t2 (n+1)
+eval' s n (Lam t)     = Lam $ eval' s (n+1) t
+eval' s n (t1 :@: t2) = case eval' s n t1 of
+                          Lam t -> eval' s n $ (Lam t) :@: t2
+                          t     -> t :@: eval' s n t2
 
 eval :: NameEnv Term -> Term -> Term
-eval env t = eval' env 0 t
-    
-    
-    
-    
-    
+eval s t = eval' s (-1) t
+
